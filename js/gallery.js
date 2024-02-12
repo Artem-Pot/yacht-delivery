022fffa83861078c204681f27a50a8ba6306835b
Artem <blbo@yandex.ru>
2024-02-12 16:23:50 +0400

    0.3.3
    Ещё один вариант галереи

--- js/gallery.js
+++ js/gallery.js
@@ -1,34 +1,14 @@
-$(function() {
-    $(".img-w").each(function() {
-      $(this).wrap("<div class='img-c'></div>")
-      let imgSrc = $(this).find("img").attr("src");
-       $(this).css('background-image', 'url(' + imgSrc + ')');
-    })
-              
-    $(".img-c").click(function() {
-      let w = $(this).outerWidth()
-      let h = $(this).outerHeight()
-      let x = $(this).offset().left
-      let y = $(this).offset().top
-      
-      
-      $(".active").not($(this)).remove()
-      let copy = $(this).clone();
-      copy.insertAfter($(this)).height(h).width(w).delay(500).addClass("active")
-      $(".active").css('top', y - 8);
-      $(".active").css('left', x - 8);
-      
-        setTimeout(function() {
-      copy.addClass("positioned")
-    }, 0)
-      
-    }) 
-  })
-  
-  $(document).on("click", ".img-c.active", function() {
-    let copy = $(this)
-    copy.removeClass("positioned active").addClass("postactive")
-    setTimeout(function() {
-      copy.remove();
-    }, 500)
-  })
+function showImage(imageSrc) {
+    let popupImage = document.getElementById("popupImage");
+    popupImage.src = imageSrc;
+     
+    let imagePopup = document.getElementById("imagePopup");
+    imagePopup.style.display = "block";
+    document.body.style.overflow = "hidden";
+  }
+
+  function closeImage() {
+    let imagePopup = document.getElementById("imagePopup");
+    imagePopup.style.display = "none";
+   document.body.style.overflow = "auto";
+  }
