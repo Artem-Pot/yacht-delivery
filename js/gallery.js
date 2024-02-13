954a96e7e04644d265b5953029004da281fc5191
Artem <blbo@yandex.ru>
2024-02-13 11:33:24 +0400

    0.3.4
    Кнопка закрытия галереи

--- js/gallery.js
+++ js/gallery.js
@@ -1,14 +1,22 @@
+let boxPopup = document.querySelector('.box-popup');
+let popupImage = document.getElementById("popupImage");
+
 function showImage(imageSrc) {
-    let popupImage = document.getElementById("popupImage");
     popupImage.src = imageSrc;
      
     let imagePopup = document.getElementById("imagePopup");
     imagePopup.style.display = "block";
+
     document.body.style.overflow = "hidden";
+
+    //окно = размеру изображения для позиционирования крестика закрытия просмотра
+    boxPopup.style.width = popupImage.clientWidth + 'px';
+    boxPopup.style.height = popupImage.clientHeight + 'px';
+
   }
 
-  function closeImage() {
-    let imagePopup = document.getElementById("imagePopup");
-    imagePopup.style.display = "none";
-   document.body.style.overflow = "auto";
-  }
+function closeImage() {
+  let imagePopup = document.getElementById("imagePopup");
+  imagePopup.style.display = "none";
+  document.body.style.overflow = "auto";
+}
