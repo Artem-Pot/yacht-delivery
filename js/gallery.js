e323da378481584b8f905addffee211d5263e08b
Artem <blbo@yandex.ru>
2024-02-13 16:32:50 +0400

    0.3.5
    Крестик работает только на маленький разрешениях, на больших изображение на весь экран

--- js/gallery.js
+++ js/gallery.js
@@ -12,7 +12,6 @@ function showImage(imageSrc) {
     //окно = размеру изображения для позиционирования крестика закрытия просмотра
     boxPopup.style.width = popupImage.clientWidth + 'px';
     boxPopup.style.height = popupImage.clientHeight + 'px';
-
   }
 
 function closeImage() {
