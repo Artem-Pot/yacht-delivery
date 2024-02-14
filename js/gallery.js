107fa9487df10510c3dd9a03441bacd8643afda1
Artem <blbo@yandex.ru>
2024-02-14 11:31:17 +0400

    0.3.6
    Адаптивный крестик галереи. Полностью рабочий.

--- js/gallery.js
+++ js/gallery.js
@@ -1,21 +1,28 @@
+let imagePopup = document.getElementById("imagePopup");
 let boxPopup = document.querySelector('.box-popup');
 let popupImage = document.getElementById("popupImage");
 
+function changingBlockSize(){
+  boxPopup.style.width = popupImage.clientWidth + 'px';
+  boxPopup.style.height = popupImage.clientHeight + 'px';
+}
+
 function showImage(imageSrc) {
     popupImage.src = imageSrc;
-     
-    let imagePopup = document.getElementById("imagePopup");
     imagePopup.style.display = "block";
-
     document.body.style.overflow = "hidden";
-
-    //окно = размеру изображения для позиционирования крестика закрытия просмотра
-    boxPopup.style.width = popupImage.clientWidth + 'px';
-    boxPopup.style.height = popupImage.clientHeight + 'px';
+    changingBlockSize();
   }
 
+//окно = размеру изображения для позиционирования крестика закрытия просмотра
+  function reportWindowSize() {
+    changingBlockSize();
+  }
+  
+  window.onresize = reportWindowSize;
+
+//закрытие просмотра изображения
 function closeImage() {
-  let imagePopup = document.getElementById("imagePopup");
   imagePopup.style.display = "none";
   document.body.style.overflow = "auto";
 }
