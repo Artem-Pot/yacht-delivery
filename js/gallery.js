31065cfb915d075a5cd80d7f9069676db46739eb
Artem <blbo@yandex.ru>
2024-02-15 11:24:25 +0400

    0.4.3
    Добавил слайдер прокрутки в галереи

--- js/gallery.js
+++ js/gallery.js
@@ -1,33 +1,71 @@
-let imagePopup = document.getElementById("imagePopup");
-let boxPopup = document.querySelector('.box-popup');
-let popupImage = document.getElementById("popupImage");
-
-function changingBlockSize(){
-  boxPopup.style.width = popupImage.clientWidth + 'px';
-  boxPopup.style.height = popupImage.clientHeight + 'px';
-}
+const imagePopup = document.getElementById("imagePopup");
+const boxPopup = document.querySelector('.box-popup');
+const popupImage = document.getElementById("popupImage");
+const next = document.querySelector('.next-button');
+const back = document.querySelector('.back-button');
+const gallery = document.querySelector('.gallery');
 
 function showImage(imageSrc) {
     popupImage.src = imageSrc;
     imagePopup.style.display = "block";
     document.body.style.overflow = "hidden";
     changingBlockSize();
-  }
+}
 
 //окно = размеру изображения для позиционирования крестика закрытия просмотра
-  function reportWindowSize() {
-    changingBlockSize();
-  }
+function reportWindowSize() {
+  changingBlockSize();
+}
   
-  window.onresize = reportWindowSize;
+function changingBlockSize(){
+  boxPopup.style.width = popupImage.clientWidth + 'px';
+  boxPopup.style.height = popupImage.clientHeight + 'px';
+}
+
+window.onresize = reportWindowSize;
 
 //закрытие просмотра изображения
 function closeImage() {
   imagePopup.style.display = "none";
   document.body.style.overflow = "auto";
 }
-//закрытие просмотра изображения в любом месте 
-// imagePopup.onclick = function() {
-//   imagePopup.style.display = "none";
-//   document.body.style.overflow = "auto";
-// }
+
+//кнопка слайдер вперёд / назад
+next.addEventListener('click', function() {
+  let imageSrc = popupImage.src.slice(0, 42); //начало url элемента 
+  let imageSrcEnd = popupImage.src.slice(42, 48); //конец url 10.jpg
+  let imageNumber = imageSrcEnd.slice(0, -4); //порядковый номер изображения
+  let newImages;
+
+  if (imageNumber < 1) {
+    newImages = `${imageSrc}${+imageNumber - 1}.jpg`;
+  }
+
+  else if (imageNumber > gallery.childElementCount - 1) {
+    newImages = `${imageSrc}1.jpg`;
+  }
+
+  else {
+    newImages = `${imageSrc}${+imageNumber + 1}.jpg`;
+  }
+
+  popupImage.src = newImages;
+})
+
+
+back.addEventListener('click', function() {
+  let imageSrc = popupImage.src.slice(0, 42); //начало url элемента 
+  let imageSrcEnd = popupImage.src.slice(42, 48); //конец url 10.jpg
+  let imageNumber = imageSrcEnd.slice(0, -4); //порядковый номер изображения
+  let newImages;
+
+  if (imageNumber < 2) {
+    newImages = `${imageSrc}${gallery.childElementCount}.jpg`;
+  }
+
+  else {
+    newImages = `${imageSrc}${+imageNumber - 1}.jpg`;
+  }
+
+  popupImage.src = newImages;
+})
