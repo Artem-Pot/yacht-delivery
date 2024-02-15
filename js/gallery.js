a642007d3014f6620d2d2379722521e3de1496f7
Artem <blbo@yandex.ru>
2024-02-15 16:17:26 +0400

    0.4.7
    Рабочий слайдер. Доделать адаптив

--- js/gallery.js
+++ js/gallery.js
@@ -30,22 +30,17 @@ function closeImage() {
   document.body.style.overflow = "auto";
 }
 
-
-
-//кнопка слайдер вперёд / назад
-next.addEventListener('click', function() {
-  let imageSrc = popupImage.src.slice(0, 58); //начало url элемента 
-  let imageSrcEnd = popupImage.src.slice(58, 64); //конец url 10.jpg
-  let imageNumber = imageSrcEnd.slice(0, -4); //порядковый номер изображения
+//кнопка слайдер вперёд слайдера
+next.addEventListener('click', function(e) {
+  let imageSrc = popupImage.src.slice(0, -6).slice(-1) === '/' ? popupImage.src.slice(0, -6) : popupImage.src.slice(0, -5); //проверка конца url на наличие /
+  let imageNumber = popupImage.src.slice(0, -6).slice(-1) === '/' ? popupImage.src.slice(-6, -4) : popupImage.src.slice(-5, -4); //порядковый номер изображения
   let newImages;
-
+  
   if (imageNumber < 1) {
     newImages = `${imageSrc}${+imageNumber - 1}.jpg`;
-    
-    
   }
 
-  else if (imageNumber > gallery.childElementCount - 1) {
+  else if (imageNumber >= gallery.childElementCount) {
     newImages = `${imageSrc}1.jpg`;
   }
 
@@ -54,14 +49,17 @@ next.addEventListener('click', function() {
   }
 
   popupImage.src = newImages;
-  console.log(newImages);
+  changingBlockSize();
+  if (event.isComposing || event.keyCode === 112) {
+    console.log('12345');
+    
+  }
 })
 
-
+//кнопка назад слайдера
 back.addEventListener('click', function() {
-  let imageSrc = popupImage.src.slice(0, 58); //начало url элемента 
-  let imageSrcEnd = popupImage.src.slice(58, 64); //конец url 10.jpg
-  let imageNumber = imageSrcEnd.slice(0, -4); //порядковый номер изображения
+  let imageSrc = popupImage.src.slice(0, -6).slice(-1) === '/' ? popupImage.src.slice(0, -6) : popupImage.src.slice(0, -5); //проверка конца url на наличие /
+  let imageNumber = popupImage.src.slice(0, -6).slice(-1) === '/' ? popupImage.src.slice(-6, -4) : popupImage.src.slice(-5, -4); //порядковый номер изображения
   let newImages;
 
   if (imageNumber < 2) {
@@ -73,4 +71,5 @@ back.addEventListener('click', function() {
   }
 
   popupImage.src = newImages;
+  changingBlockSize();
 })
