7fe8bd0b3360ea00bc057c4cdfb5284ea3c78e28
Artem <blbo@yandex.ru>
2024-02-28 16:28:22 +0400

    0.7.12
    тест слайдера

--- js/swiper.js
+++ js/swiper.js
@@ -1,30 +1,25 @@
 const swiper = new Swiper('.swiper', {
     loop: true, //зацикливание слайдов до бесконечности
     grabCursor: true, //изменит курсор для перетаскивания
-    slideToClickedSlide: true, //переключает на следующий слайд при клике больше 50% от ширины слайда
-    speed: 1000, //скорость переключения слайда
-
-    forceToAxis: false,
-    releaseOnEdges: false,
-    sensitivity: 10,
+    speed: 1500, //скорость переключения слайда
+ 
     effect: 'fade', //эффект прозрачности при смене слайдов
     fadeEffect: {
         crossFade: true, //дополнение к эффекту fade - параллельная смена прозрачности, добавляет плавность
     },
   
-    // нумерация страниц
-    pagination: {
+    direction: 'horizontal', //горизонтальный слайдер
+
+    pagination: {     // нумерация страниц
       el: '.swiper-pagination',
     },
   
-    // Кнопки навигации
-    navigation: {
+    navigation: {     // Кнопки навигации
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
   
-    // Полоса прокрутки
-    scrollbar: {
+    scrollbar: {    // Полоса прокрутки
       el: '.swiper-scrollbar',
     },
 
@@ -32,4 +27,14 @@ const swiper = new Swiper('.swiper', {
         delay: 10000, //паузка между прокруткой
         disableOnInteraction: true, // отключить при ручном переключении
     },
+
+    keyboard: { //прокрутка с помощью клавиатуры
+      enabled: true,
+      onlyInViewport: false,
+    },
+
+    mousewheel: { //отключение прокрутки с помощью мыши
+      invert: false,
+    },
+    // autoHeight: true,
   });
